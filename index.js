199aa100dbe86e14a0418861bc5616900ca6adc8
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-09-03 16:38:04 -0700

    adding in questions

--- index.js
+++ index.js
@@ -2,14 +2,53 @@
 const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = ['What is your project title?', 
-'Give a brief project description', 
-'Do you have specific installation instructions/Requirements?', 
-'What is your project used for?', 
-'What license are you using?',
-'Who contributed to this project', 
-'Specify and QA testing',
-'GitHub username?'];
+const questions = inquirer.prompt([
+   {
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title'
+   } , 
+   {
+    type: 'input',
+    message: 'Give a brief project description',
+    name: 'description'
+   } ,
+   {
+    type: 'input',
+    message: 'Describe any specific installation instructions/Requirements?',
+    name: 'instructions'
+   } ,
+   {
+    type: 'input',
+    message: 'What is your project used for?',
+    name: 'usage'
+   } ,
+   {
+    type: 'dropdown',
+    message: 'What license are you using?',
+    name: 'license'
+   } ,
+   {
+    type: 'input',
+    message: 'List any contributors or code sources',
+    name: 'contributors'
+   } ,
+   {
+    type: 'input',
+    message: 'Specify any code tests',
+    name: 'tests'
+   } ,
+   {
+    type: 'input',
+    message: 'GitHub username?',
+    name: 'github'
+   } ,
+   {
+    type: 'input',
+    message: 'Email address?',
+    name: 'email'
+   } ,
+,]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
