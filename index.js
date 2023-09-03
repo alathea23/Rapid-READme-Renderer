3d36f52563f23fc8576762bf113393f658c771b9
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-09-03 16:24:34 -0700

    adding license info and logic" git push

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your project title?', 
+'Give a brief project description', 
+'Do you have specific installation instructions/Requirements?', 
+'What is your project used for?', 
+'What license are you using?',
+'Who contributed to this project', 
+'Specify and QA testing',
+'GitHub username?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +19,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+const port = process.env.PORT || 3001
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license.license; // i think this is problem #1
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'Apache') {
+    yourLicense = `![Apache license](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+  }else if (licenseType === 'Eclipse') {
+    yourLicense = `![Eclipse license](https://img.shields.io/badge/License-EPL%201.0-red.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+  } else if (licenseType === 'Mozilla') {
+    yourLicense = `![Mozilla license](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`
+  } else if (licenseType === 'Perl') {
+    yourLicense = `![Perl license](https://img.shields.io/badge/License-Perl-0298c3.svg)`
+  } else {
+    license.license = "N/A"
+    yourLicense = ""
+  }
+  return yourLicense;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseType = license.license; // i think this is problem #1
+  let licenseLink = ''
+  if(licenseType === 'MIT') {
+    licenseLink = `(https://opensource.org/licenses/MIT)`
+  } else if (licenseType === 'Apache') {
+    licenseLink = `(https://opensource.org/licenses/Apache-2.0)`
+  }else if (licenseType === 'Eclipse') {
+    licenseLink = `(https://opensource.org/licenses/EPL-1.0)`
+  } else if (licenseType === 'GPLv3') {
+    licenseLink = `(https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (licenseType === 'Mozilla') {
+    licenseLink = `(https://opensource.org/licenses/MPL-2.0)`
+  } else if (licenseType === 'Perl') {
+    licenseLink = `(https://opensource.org/licenses/Artistic-2.0)`
+  } else {
+    license.license = "N/A"
+    licenseLink = ""
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
