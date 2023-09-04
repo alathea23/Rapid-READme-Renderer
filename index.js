d4f5f64e111583d9b9361319d550277486c08cb8
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-09-03 22:11:07 -0700

    starting to add in write file logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = inquirer.prompt([
@@ -52,7 +53,11 @@ const questions = inquirer.prompt([
 ,]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+       err ? console.error(err) : console.log('Professional README created!')
+     );
+}
 
 // TODO: Create a function to initialize app
 function init() {
