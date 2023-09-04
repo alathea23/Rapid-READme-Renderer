4d912f659dcbe0b12d7c573e45a151a472b1f4bc
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-09-04 13:41:55 -0700

    debugging questionairre functionality; adding asyncronous function

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// TODO: Include packages needed for this application
+//Setting up packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-inquirer
+//Creating an array of questions for user input
+const questions = inquirer
 .prompt([
+    {
+        type: 'list',
+        message: 'What license are you using?',
+        choices: ["MIT", "Apache", "Eclipse", "GPLv3", "GPL", "Mozilla", "Perl", "N/A"],
+        name: 'license'
+    },
     {
         type: 'input',
         message: 'What is your project title?',
@@ -27,12 +33,6 @@ inquirer
         name: 'usage'
     },
     {
-        type: 'dropdown',
-        message: 'What license are you using?',
-        choices: ["MIT", "Apache", "Eclipse", "GPLv3", "GPL", "Mozilla", "Perl", "N/A"],
-        name: 'license'
-    },
-    {
         type: 'input',
         message: 'List any contributors or code sources',
         name: 'contributors'
@@ -51,25 +51,29 @@ inquirer
         type: 'input',
         message: 'Email address?',
         name: 'email'
-    }]);
-    then((response) => 
-    console.log(response));
+    }])
+    .then((data) => (
+    answers = data,
+    console.log(answers)));
 
 
-// TODO: Create a function to write README file
-function writeFile(fileName, response) {
-    fileName = response.title + README.md
+//function to write output to README file
+function writeFile(fileName, answers) {
+    fileName = answers.title + 'README.md'
+    //console to test code
+    console.log(fileName)
 
-    fs.writeFile(fileName, data, (err) =>
+    fs.writeFile(fileName, answers, (err) =>
         err ? console.error(err) : console.log('Professional README created!')
     )
 };
 
 
-// TODO: Create a function to initialize app
-function init() {
+//function to initialize app
+//source for async function fix for command line: https://stackoverflow.com/questions/62860243/inquirer-prompt-exiting-without-an-answer
+async function init() {
     const port = process.env.PORT || 3001
-    questions
+    await questions
     writeFile()
 }
 
